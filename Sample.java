dc6211e1ec5234dcaa871871a22f32301523dc41
swathi singireddy <ssreddy175@gmail.com>
2016-05-03 21:30:50 -0700

    bug1 code changes

--- Sample.java
+++ Sample.java
@@ -2,5 +2,8 @@ public class Sample{
 
 public static void main(String args[]){
     System.out.println("Hello World!");
+}
+public static void display(){
+    System.out.println("Another printf");
   }
 }
